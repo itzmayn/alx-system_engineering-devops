f5f05e93fad497f7351d9532ddf658384df06101
Mayn <123576452+itzmayn@users.noreply.github.com>
2023-09-23 00:04:42 +0000

    m

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
 # Ensure that puppet-lint is installed at version 2.1.1 using the gem provider
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install Flask==2.1.0',
+  unless  => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
